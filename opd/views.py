3787f149b96a7a038af6bfe34afbc4288b37ddd1
Rasel S Str <54805845+raselstr@users.noreply.github.com>
2024-05-17 18:30:05 +0700

    OPD List

--- opd/views.py
+++ opd/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
 from .models import Opd
 
-def index(request):
-    # if request.method == "GET":
-        return render(request, "opd/opd.html")
-
 # Create your views here.
 def opd_list(request):
     opd = Opd.objects.all()
-    return render(request, "opd/opd_list.html", {"opd": opd})
+    return render(request, "opd/opd_list.html", {"opds": opd})
 
