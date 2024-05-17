1adb1f7e122f5a832468d079dc386ce8009b30b9
Rasel S Str <54805845+raselstr@users.noreply.github.com>
2024-05-17 17:35:49 +0700

    tambah model OPD

--- opd/models.py
+++ opd/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Opd(models.Model):
+    kode_opd = models.CharField(max_length=10)
+    nama_opd = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nama_opd
+
