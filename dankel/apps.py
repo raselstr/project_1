5c09a18118710237895058e2fccfa0e713b7ffac
Rasel S Str <54805845+raselstr@users.noreply.github.com>
2024-06-14 11:23:09 +0700

    mulai untuk dankel

--- dankel/apps.py
+++ dankel/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class DankelConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dankel'
+    
+    def ready(self):
+        import signals.models_signals
---
+++ dankel/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+               
+    # path("rencana/delete/<int:pk>", views.delete, name="delete_rencana"),
+    # path("rencana/update/<int:pk>", views.update, name="update_rencana"),
+    # path("rencana/simpan", views.simpan, name="simpan_rencana"),
+    # path("rencana/", views.list, name="list_rencana"),
+    
+]
