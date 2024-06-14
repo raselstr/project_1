90d61c5879f6f1094b368712b673c0dbc7cfe11d
Rasel S Str <54805845+raselstr@users.noreply.github.com>
2024-06-14 20:02:08 +0700

    rencana Fix

--- dankel/urls.py
+++ dankel/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
                
     # path("rencana/delete/<int:pk>", views.delete, name="delete_rencana"),
     # path("rencana/update/<int:pk>", views.update, name="update_rencana"),
-    path('simpan/', views.rencdankel_simpan, name="rencdankel_simpan"),
-    path('form/', views.rencdankel_form, name="rencdankel_form"),
-    path('', views.rencdankel_list, name='rencdankel_list'),
+    path('delete/<int:pk>', views.delete, name="rencdankel_delete"),
+    path('update/<int:pk>', views.update, name="rencdankel_update"),
+    path('simpan/', views.simpan, name="rencdankel_simpan"),
+    path('', views.list, name='rencdankel_list'),
     
 ]
--- dankel/views.py
+++ dankel/views.py
@@ -1,41 +1,40 @@
 # File: your_app/views.py
 from django.shortcuts import render, get_object_or_404,redirect
+from django.core.exceptions import ValidationError
+from django.contrib import messages
 from .models import RencDankel
 from .forms import RencDankelForm, RencDankelsisaFormSet
 
-# def rencdankel_form(request):
-#     if request.method == 'POST':
-#         form = RencDankelForm(request.POST)
-#         formset = RencDankelsisaFormSet(request.POST)
-#         if form.is_valid() and formset.is_valid():
-#             rencdankel = form.save()
-#             instances = formset.save(commit=False)
-#             for instance in instances:
-#                 instance.rencdankelsisa_rencana = rencdankel
-#                 instance.save()
-#             return redirect('success_url')  # Ganti dengan URL redirect setelah berhasil
-#     else:
-#         form = RencDankelForm()
-#         formset = RencDankelsisaFormSet()
-    
-#     context = {
-#         'form': form,
-#         'formset': formset,
-#         'judul' : 'Form Rencana Kegiatan'
-#     }
-#     return render(request, 'dankel/dankel_form.html', context)
 
-def rencdankel_update(request, pk):
+Model_data = RencDankel
+Form_data = RencDankelForm
+tag_url = 'rencdankel_list'
+template = 'dankel/dankel_form.html'
+template_list = 'dankel/dankel_list.html'
+
+def delete(request, pk):
+    try:
+        data = Model_data.objects.get(id=pk)
+        data.delete()
+        messages.warning(request, "Data Berhasil dihapus")
+    except Model_data.DoesNotExist:
+        messages.error(request,"Dana tidak ditemukan")
+    except ValidationError as e:
+        messages.error(request, str(e))
+    return redirect(tag_url)
+
+def update(request, pk):
     # Mendapatkan instance RencDankel yang ingin diupdate
-    rencdankel = get_object_or_404(RencDankel, pk=pk)
+    rencdankel = get_object_or_404(Model_data, pk=pk)
 
     if request.method == 'POST':
         # Membuat instance formulir dengan data yang dikirimkan
-        form = RencDankelForm(request.POST, instance=rencdankel)
+        form = Form_data(request.POST, instance=rencdankel)
         formset = RencDankelsisaFormSet(request.POST, instance=rencdankel)
         if form.is_valid() and formset.is_valid():
             # Menyimpan perubahan pada objek RencDankel
             rencdankel = form.save()
+            messages.success(request, 'Data Berhasil Update')
 
             # Menyimpan perubahan pada setiap objek RencDankelsisa yang terkait
             instances = formset.save(commit=False)
@@ -43,22 +42,25 @@ def rencdankel_update(request, pk):
                 instance.rencdankelsisa_rencana = rencdankel
                 instance.save()
 
-            return redirect('success_url')  # Ganti dengan URL redirect setelah berhasil
+            return redirect(tag_url)  # Ganti dengan URL redirect setelah berhasil
     else:
         # Menyiapkan formulir dan formset dengan data yang sudah ada
-        form = RencDankelForm(instance=rencdankel)
+        form = Form_data(instance=rencdankel)
         formset = RencDankelsisaFormSet(instance=rencdankel)
     
     context = {
         'form': form,
         'formset': formset,
+        'judul': 'Update Rencana Kegiatan',
+        'btntombol' : 'Update',
     }
-    return render(request, 'dankel/dankel_form.html', context)
+    return render(request, template, context)
 
 
-def rencdankel_simpan(request):
+def simpan(request):
+    
     if request.method == 'POST':
-        form = RencDankelForm(request.POST)
+        form = Form_data(request.POST)
         formset = RencDankelsisaFormSet(request.POST)
         if form.is_valid() and formset.is_valid():
             rencdankel = form.save()
@@ -66,33 +68,23 @@ def rencdankel_simpan(request):
             for instance in instances:
                 instance.rencdankelsisa_rencana = rencdankel
                 instance.save()
-            formset.save_m2m()
-            return redirect('rencdankel_list')  # Ganti dengan URL redirect setelah berhasil
+            messages.success(request, 'Data Berhasil Simpan')
+            return redirect(tag_url)  # Ganti dengan URL redirect setelah berhasil
     else:
-        form = RencDankelForm()
+        form = Form_data()
         formset = RencDankelsisaFormSet()
     
     context = {
         'form': form,
         'formset': formset,
-        'judul': 'Form Rencana Kegiatan'
+        'judul': 'Form Rencana Kegiatan',
+        'btntombol' : 'Simpan',
+        
     }
-    return render(request, 'dankel/dankel_form.html', context)
+    return render(request, template, context)
 
-
-def rencdankel_form(request):
-    form = RencDankelForm()
-    formset = RencDankelsisaFormSet()
-    
-    context = {
-        'form': form,
-        'formset': formset,
-        'judul': 'Form Rencana Kegiatan'
-    }
-    return render(request, 'dankel/dankel_form.html', context)
-
-def rencdankel_list(request):
-    rencdankels = RencDankel.objects.select_related(
+def list(request):
+    rencdankels = Model_data.objects.select_related(
         'rencdankel_opd', 
         'rencdankel_sub__dankelsub_keg'
         ).prefetch_related('rencdankelsisa').all().order_by('rencdankel_sub__dankelsub_keg')
@@ -101,5 +93,6 @@ def rencdankel_list(request):
         'rencdankels': rencdankels,
         'judul' : 'Rencana Kegiatan',
         'tombol' : 'Tambah Perencanaan',
+        
     }
-    return render(request, 'dankel/dankel_list.html', context)
+    return render(request, template_list, context)
