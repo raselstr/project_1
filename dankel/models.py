03939b5e905ae91889574020dc1395f2f8f0b638
Rasel S Str <54805845+raselstr@users.noreply.github.com>
2024-06-21 18:28:28 +0700

    update required

--- dankel/models.py
+++ dankel/models.py
@@ -7,12 +7,18 @@ from datetime import datetime
 
 # Create your models here.
 class RencDankel(models.Model):
+    VERIF = [
+        (0, 'Input Dinas'),
+        (1, 'Disetujui'),
+        (2, 'Ditolak')
+    ]
     rencdankel_tahun = models.IntegerField(verbose_name="Tahun",default=datetime.now().year)
     rencdankel_subopd = models.ForeignKey(Subopd, verbose_name='Sub Opd',on_delete=models.CASCADE)
     rencdankel_sub = models.ForeignKey(Dankelsub, verbose_name='Sub Kegiatan', on_delete=models.CASCADE)
     rencdankel_pagu = models.DecimalField(verbose_name='Pagu Anggaran',max_digits=17, decimal_places=2,default=0)
     rencdankel_output = models.DecimalField(verbose_name='Output',max_digits=8, decimal_places=2,default=0)
     rencdankel_ket = models.TextField(verbose_name='Keterangan Kegiatan', blank=True)
+    rencdankel_verif = models.IntegerField(choices=VERIF, default = 0)
     
     class Meta:
         constraints = [
@@ -32,10 +38,16 @@ class RencDankel(models.Model):
         return self.rencdankel_ket
 
 class RencDankelsisa(models.Model):
+    VERIF = [
+        (0, 'Input Dinas'),
+        (1, 'Disetujui'),
+        (2, 'Ditolak')
+    ]
     rencdankelsisa_rencana = models.OneToOneField(RencDankel, verbose_name='Rencana Kegiatan', on_delete=models.CASCADE, related_name='rencdankelsisa')
     rencdankelsisa_pagu = models.DecimalField(verbose_name='Pagu Anggaran Sisa',max_digits=17, decimal_places=2,default=0, blank=True)
     rencdankelsisa_output = models.DecimalField(verbose_name='Output Sisa',max_digits=8, decimal_places=2,default=0, blank=True)
-    rencdankelsisa_ket = models.TextField(verbose_name='Keterangan Kegiatan Sisa', blank=True) 
+    rencdankelsisa_ket = models.TextField(verbose_name='Keterangan Kegiatan Sisa', blank=True)
+    rencdankelsisa_verif = models.IntegerField(choices=VERIF, default = 0) 
     
     def __str__(self):
         return self.rencdankelsisa_ket
