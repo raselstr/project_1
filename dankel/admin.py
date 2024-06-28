80137737a51992768675c03988433a486da5e2a0
Rasel S Str <54805845+raselstr@users.noreply.github.com>
2024-06-28 20:41:49 +0700

    input rencana dari admin

--- dankel/admin.py
+++ dankel/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from .models import RencDankel, RencDankelsisa
 
-# class RencDankelsisaInline(admin.TabularInline):
-#     model = RencDankelsisa
-#     extra = 1  # Jumlah form tambahan yang ingin ditampilkan secara default
+class RencDankelAdmin(admin.ModelAdmin):
+    list_display = ('rencdankel_tahun','rencdankel_dana', 'rencdankel_subopd', 'rencdankel_sub', 'rencdankel_pagu', 'rencdankel_output', 'rencdankel_verif')
 
-# @admin.register(RencDankel)
-# class RencDankelAdmin(admin.ModelAdmin):
-#     inlines = [RencDankelsisaInline]
+class RencDankelsisaAdmin(admin.ModelAdmin):
+    list_display = ('rencdankelsisa_tahun','rencdankelsisa_dana', 'rencdankelsisa_subopd', 'rencdankelsisa_sub', 'rencdankelsisa_pagu', 'rencdankelsisa_output', 'rencdankelsisa_verif')
+
+admin.site.register(RencDankel, RencDankelAdmin)
+admin.site.register(RencDankelsisa,RencDankelsisaAdmin)
+
--- dankel/models.py
+++ dankel/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models import UniqueConstraint
 from django.core.exceptions import ValidationError
 from opd.models import Subopd
+from dana.models import Subrinc
 from dausg.models import Dankelsub
 from datetime import datetime
 
@@ -13,6 +14,7 @@ class RencDankel(models.Model):
         (2, 'Ditolak')
     ]
     rencdankel_tahun = models.IntegerField(verbose_name="Tahun",default=datetime.now().year)
+    rencdankel_dana = models.ForeignKey(Subrinc, verbose_name='Sumber Dana',on_delete=models.CASCADE,editable=False)
     rencdankel_subopd = models.ForeignKey(Subopd, verbose_name='Sub Opd',on_delete=models.CASCADE)
     rencdankel_sub = models.ForeignKey(Dankelsub, verbose_name='Sub Kegiatan', on_delete=models.CASCADE)
     rencdankel_pagu = models.DecimalField(verbose_name='Pagu Anggaran',max_digits=17, decimal_places=2,default=0)
@@ -34,6 +36,15 @@ class RencDankel(models.Model):
         ).exclude(pk=self.pk).exists():
             raise ValidationError('Rencana Kegiatan untuk Tahun, Sub Opd dan Sub Kegiatan ini sudah ada, silahkan masukkan yang lain.')
     
+    def save(self, *args, **kwargs):
+        # Set default value for rencdankel_dana if not provided
+        if not self.rencdankel_dana_id:
+            try:
+                self.rencdankel_dana = Subrinc.objects.get(subrinc_slug='dana-kelurahan')
+            except Subrinc.DoesNotExist:
+                raise ValidationError('Sumber Dana dengan slug "dana-kelurahan" tidak ditemukan.')
+        super(RencDankel, self).save(*args, **kwargs)
+    
     def __str__(self):
         return self.rencdankel_ket
 
@@ -44,12 +55,36 @@ class RencDankelsisa(models.Model):
         (2, 'Ditolak')
     ]
     rencdankelsisa_tahun = models.IntegerField(verbose_name="Tahun",default=datetime.now().year)
+    rencdankelsisa_dana = models.ForeignKey(Subrinc, verbose_name='Sumber Dana',on_delete=models.CASCADE,editable=False)
     rencdankelsisa_subopd = models.ForeignKey(Subopd, verbose_name='Sub Opd',on_delete=models.CASCADE)
     rencdankelsisa_sub = models.ForeignKey(Dankelsub, verbose_name='Sub Kegiatan', on_delete=models.CASCADE)
     rencdankelsisa_pagu = models.DecimalField(verbose_name='Pagu Anggaran Sisa',max_digits=17, decimal_places=2,default=0, blank=True)
     rencdankelsisa_output = models.DecimalField(verbose_name='Output Sisa',max_digits=8, decimal_places=2,default=0, blank=True)
     rencdankelsisa_ket = models.TextField(verbose_name='Keterangan Kegiatan Sisa', blank=True)
     rencdankelsisa_verif = models.IntegerField(choices=VERIF, default = 0, editable=False) 
+    
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=['rencdankelsisa_tahun', 'rencdankelsisa_subopd', 'rencdankelsisa_sub'], name='unique_rencdankelsisa')
+        ]
+    
+    def clean(self):
+        # Check if the combination already exists
+        if RencDankelsisa.objects.filter(
+            rencdankelsisa_tahun=self.rencdankelsisa_tahun,
+            rencdankelsisa_subopd=self.rencdankelsisa_subopd,
+            rencdankelsisa_sub=self.rencdankelsisa_sub
+        ).exclude(pk=self.pk).exists():
+            raise ValidationError('Rencana Kegiatan untuk Tahun, Sub Opd dan Sub Kegiatan ini sudah ada, silahkan masukkan yang lain.')
+    
+    def save(self, *args, **kwargs):
+        # Set default value for rencdankel_dana if not provided
+        if not self.rencdankelsisa_dana_id:
+            try:
+                self.rencdankelsisa_dana = Subrinc.objects.get(subrinc_slug='dana-kelurahan')
+            except Subrinc.DoesNotExist:
+                raise ValidationError('Sumber Dana dengan slug "dana-kelurahan" tidak ditemukan.')
+        super(RencDankelsisa, self).save(*args, **kwargs)
     
     def __str__(self):
         return self.rencdankelsisa_ket
