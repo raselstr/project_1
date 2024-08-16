9f7cfd9d5604151484e3ee9f4212401c19f1f676
Rasel S Str <54805845+raselstr@users.noreply.github.com>
2024-08-16 15:31:01 +0700

    view reisasisisa tanda tidak sama dengan

--- dankel/views/view_realisasisisa.py
+++ dankel/views/view_realisasisisa.py
@@ -136,7 +136,7 @@ def list(request):
         filters &= Q(realisasidankelsisa_dana_id=danarealisasi_id)
     if tahaprealisasi_id:
         filters &= Q(realisasidankelsisa_tahap_id=tahaprealisasi_id)
-    if subopdrealisasi_id is not 125:
+    if subopdrealisasi_id != 125:
         filters &= Q(realisasidankelsisa_subopd_id=subopdrealisasi_id)
     
     # Terapkan filter ke query data
