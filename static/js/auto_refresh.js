df35497900e3c3b0f8921d7e7223d6c8035f04ef
Rasel S Str <54805845+raselstr@users.noreply.github.com>
2024-09-06 19:07:27 +0700

    taambah keterangan console

--- static/js/auto_refresh.js
+++ static/js/auto_refresh.js
@@ -1,6 +1,7 @@
 // static/js/auto_refresh.js
 
 function autoRefreshPage() {
+    console.log("Halaman akan di-refresh setelah 1 menit");
     setTimeout(function() {
         window.location.reload();
     }, 60000); // 60000 milliseconds = 1 minute
