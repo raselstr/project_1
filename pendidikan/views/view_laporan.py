4ae660d564c34e89bb13d163f5238b95b4113459
Rasel S Str <54805845+raselstr@users.noreply.github.com>
2024-09-21 12:47:07 +0700

    laporan sudah OK DAU Pendidikan

--- pendidikan/views/view_laporan.py
+++ pendidikan/views/view_laporan.py
@@ -4,6 +4,7 @@ from django.db.models import Q,Sum, DecimalField
 from django.urls import reverse
 from django.contrib import messages
 from project.decorators import menu_access_required, set_submenu_session
+from django.db.models import Prefetch
 
 from decimal import Decimal
 
@@ -14,7 +15,7 @@ import logging
 import string
 
 from pendidikan.models import Rencanaposting, Rencana, Realisasi
-from dausg.models import Subkegiatan
+from dausg.models import Subkegiatan, DausgpendidikanProg
 
 
 from pendidikan.forms import RealisasiFilterForm, RealisasiForm
@@ -30,6 +31,7 @@ model_pagu = Rencana
 model_dana = Subkegiatan
 model_realisasi = Realisasi
 model_penerimaan = Penerimaan
+model_program = DausgpendidikanProg
 
 url_home = 'laporan_pendidikan_home'
 url_filter = 'laporan_pendidikan_filter'
@@ -131,37 +133,15 @@ def simpan(request):
 @menu_access_required('list')
 def list(request):
     request.session['next'] = request.get_full_path()
-    realisasi_tahun=request.session.get('realisasi_tahun')
-    realisasi_dana=request.session.get('realisasi_dana')
-    realisasi_subopd=request.session.get('realisasi_subopd')
-    realisasi_tahap=request.session.get('realisasi_tahap')
-     # Buat filter query
-    filters = Q()
-    if realisasi_tahun:
-        filters &= Q(realisasi_tahun=realisasi_tahun)
-    if realisasi_dana:
-        filters &= Q(realisasi_dana_id=realisasi_dana)
-    if realisasi_tahap:
-        filters &= Q(realisasi_tahap_id=realisasi_tahap)
-    if realisasi_subopd not in [124]:
-        filters &= Q(realisasi_subopd_id=realisasi_subopd)
+    context = get_data_context(request)
+    level = request.session.get('level')
     
-    try:
-        data = model_realisasi.objects.filter(filters)
-    except model_realisasi.DoesNotExist:
-        data = None
-    
-    table = tabel_realisasi(data)
-    RequestConfig(request, paginate={"per_page": 25}).configure(table)
-
-    context = {
-        'judul': 'Laporan DAU Bidang Pendidikan',
-        'tombol': 'Tambah Realisasi',
-        'kembali' : 'Kembali',
-        'link_url_kembali': reverse(url_home),
-        'data' : data,
-        'table':table,
-    }
+    context.update({
+        'judul': 'Rekapitulasi Realisasi DAU SG Bidang Pendidikan',
+        'tombol': 'Cetak',
+        'tombolsp2d': 'Cetak Daftar SP2D',
+        'level' : level
+    })
     return render(request, template_list, context)
 
 
@@ -237,10 +217,11 @@ def home(request):
     return render(request, template_home, context)
 
 def get_data_context(request):
-    realisasi_tahun=request.session.get('realisasi_tahun')
-    realisasi_dana=request.session.get('realisasi_dana')
-    realisasi_subopd=request.session.get('realisasi_subopd')
-    realisasi_tahap=request.session.get('realisasi_tahap')
+    # Ambil data dari session
+    realisasi_tahun = request.session.get('realisasi_tahun')
+    realisasi_dana = request.session.get('realisasi_dana')
+    realisasi_subopd = request.session.get('realisasi_subopd')
+    realisasi_tahap = request.session.get('realisasi_tahap')
     jadwal = request.session.get('jadwal')
     level = request.session.get('level')
 
@@ -252,11 +233,11 @@ def get_data_context(request):
         filters &= Q(posting_tahun=realisasi_tahun)
     if realisasi_dana:
         filters &= Q(posting_dana_id=realisasi_dana)
-    if realisasi_subopd not in [124,67,70]:
+    if realisasi_subopd and realisasi_subopd not in [124, 67, 70]:
         filters &= Q(posting_subopd_id=realisasi_subopd)
-    
+
     filterreals = Q()
-    if level == 'APIP' :
+    if level == 'APIP':
         filterreals &= Q(realisasidankel_verif=1)
     if realisasi_tahun:
         filterreals &= Q(realisasi_tahun=realisasi_tahun)
@@ -264,13 +245,16 @@ def get_data_context(request):
         filterreals &= Q(realisasi_dana_id=realisasi_dana)
     if realisasi_tahap:
         filterreals &= Q(realisasi_tahap_id=realisasi_tahap)
-    if realisasi_subopd not in [124,67,70]:
+    if realisasi_subopd and realisasi_subopd not in [124, 67, 70]:
         filterreals &= Q(realisasi_subopd_id=realisasi_subopd)
-    
-    progs = model_data.objects.filter(filters)
+
+    progs = model_program.objects.prefetch_related(
+        Prefetch('dausgpendidikankegs__dausgpendidikansubs__rencanaposting_set')
+    ).filter(dausgpendidikankegs__dausgpendidikansubs__rencanaposting__isnull=False).distinct()
+
     rencanas = model_data.objects.filter(filters)
     realisasis = model_realisasi.objects.filter(filterreals)
-    
+
     # Siapkan data untuk template
     prog_data = []
     total_pagu_keseluruhan = 0
@@ -278,8 +262,8 @@ def get_data_context(request):
     total_realisasi_keseluruhan = 0
     total_realisasi_output_keseluruhan = 0
 
-    # Tambahkan variabel untuk penomoran
-    program_counter = 1  # Inisialisasi angka program (numerik)
+    # Di dalam get_data_context
+    program_counter = 1
 
     for prog in progs:
         total_pagu_prog = 0
@@ -287,90 +271,89 @@ def get_data_context(request):
         total_realisasi_prog = 0
         total_realisasi_output_prog = 0
         prog_kegs = []
+        kegiatan_counter = 1  # Nomor urut kegiatan
 
-        # Tambahkan variabel untuk penomoran kegiatan
-        kegiatan_counter = 0  # Inisialisasi huruf kegiatan
-        alphabet_list = list(string.ascii_uppercase)  # Daftar alfabet A-Z
-
-        for keg in prog.posting_subkegiatan.all():
+        for keg in prog.dausgpendidikankegs.all():
             total_pagu_keg = 0
             total_output_keg = 0
             total_realisasi_keg = 0
             total_realisasi_output_keg = 0
             keg_subs = []
-            for sub in keg.dankelsubs.all():
-                related_rencanas = rencanas.filter(rencdankel_sub=sub)
+            sub_kegiatan_counter = 1  # Nomor urut sub kegiatan
 
-                # Ambil data pagu dan output
-                pagu = 0
-                output = 0
+            for sub in keg.dausgpendidikansubs.all():
+                related_rencanas = rencanas.filter(posting_subkegiatan=sub)
+
                 if related_rencanas.exists():
                     pagu_output = related_rencanas.aggregate(
-                        total_pagu=Sum('rencdankel_pagu'),
-                        total_output=Sum('rencdankel_output')
+                        total_pagu=Sum('posting_pagu'),
+                        total_output=Sum('posting_output')
                     )
                     pagu = pagu_output['total_pagu'] or 0
                     output = pagu_output['total_output'] or 0
 
-                total_pagu_keg += pagu
-                total_output_keg += output
+                    if pagu > 0:  # Hanya jika ada pagu
+                        total_pagu_keg += pagu
+                        total_output_keg += output
 
-                # Ambil realisasi terkait dengan `realisasidankel_idrencana`
-                total_sp2d = 0
-                total_output_realisasi = 0
-                for rencana in related_rencanas:
-                    realisasi_rencana = realisasis.filter(realisasidankel_rencana=rencana.id)
-                    total_sp2d += realisasi_rencana.aggregate(total_sp2d=Sum('realisasidankel_sp2dnilai'))['total_sp2d'] or 0
-                    total_output_realisasi += realisasi_rencana.aggregate(total_output=Sum('realisasidankel_output'))['total_output'] or 0
-                keg_subs.append({
-                    'sub': sub,
-                    'pagu': pagu,
-                    'output': output,
-                    'realisasi': {
-                        'total_sp2d': total_sp2d,
-                        'total_output': total_output_realisasi
-                    }
+                        total_sp2d = 0
+                        total_output_realisasi = 0
+                        for rencana in related_rencanas:
+                            realisasi_rencana = realisasis.filter(realisasi_rencanaposting_id=rencana.id)
+                            total_sp2d += realisasi_rencana.aggregate(total_sp2d=Sum('realisasi_nilai'))['total_sp2d'] or 0
+                            total_output_realisasi += realisasi_rencana.aggregate(total_output=Sum('realisasi_output'))['total_output'] or 0
+
+                        keg_subs.append({
+                            'sub': sub,
+                            'pagu': pagu,
+                            'output': output,
+                            'realisasi': {
+                                'total_sp2d': total_sp2d,
+                                'total_output': total_output_realisasi
+                            },
+                            'sub_number': f"{program_counter}.{kegiatan_counter}.{sub_kegiatan_counter}"  # Nomor sub kegiatan
+                        })
+
+                        total_realisasi_output_keg += total_output_realisasi
+                        total_realisasi_keg += total_sp2d
+
+                sub_kegiatan_counter += 1  # Increment sub kegiatan counter
+
+            if total_pagu_keg > 0:  # Hanya jika ada total pagu
+                prog_kegs.append({
+                    'keg': keg,
+                    'subs': keg_subs,
+                    'total_pagu_keg': total_pagu_keg,
+                    'total_output_keg': total_output_keg,
+                    'total_realisasi_keg': total_realisasi_keg,
+                    'total_realisasi_output_keg': total_realisasi_output_keg,
+                    'kegiatan_number': f"{program_counter}.{kegiatan_counter}"  # Nomor kegiatan
                 })
 
-                total_realisasi_output_keg += total_output_realisasi
-                total_realisasi_keg += total_sp2d
+                total_pagu_prog += total_pagu_keg
+                total_output_prog += total_output_keg
+                total_realisasi_prog += total_realisasi_keg
+                total_realisasi_output_prog += total_realisasi_output_keg
 
-            # Gunakan huruf alphabet untuk kegiatan
-            kegiatan_number = alphabet_list[kegiatan_counter]
-            kegiatan_counter += 1
+                kegiatan_counter += 1  # Increment kegiatan counter
 
-            prog_kegs.append({
-                'kegiatan_number': kegiatan_number,  # Nomor kegiatan dengan huruf
-                'keg': keg,
-                'subs': keg_subs,
-                'total_pagu_keg': total_pagu_keg,
-                'total_output_keg': total_output_keg,
-                'total_realisasi_keg': total_realisasi_keg,
-                'total_realisasi_output_keg': total_realisasi_output_keg
+        if total_pagu_prog > 0:  # Hanya jika ada total pagu
+            prog_data.append({
+                'prog_number': program_counter,
+                'prog': prog,
+                'kegs': prog_kegs,
+                'total_pagu_prog': total_pagu_prog,
+                'total_output_prog': total_output_prog,
+                'total_realisasi_prog': total_realisasi_prog,
+                'total_realisasi_output_prog': total_realisasi_output_prog
             })
-            total_pagu_prog += total_pagu_keg
-            total_output_prog += total_output_keg
-            total_realisasi_prog += total_realisasi_keg
-            total_realisasi_output_prog += total_realisasi_output_keg
 
-        total_pagu_keseluruhan += total_pagu_prog
-        total_output_keseluruhan += total_output_prog
-        total_realisasi_keseluruhan += total_realisasi_prog
-        total_realisasi_output_keseluruhan += total_realisasi_output_prog
+            total_pagu_keseluruhan += total_pagu_prog
+            total_output_keseluruhan += total_output_prog
+            total_realisasi_keseluruhan += total_realisasi_prog
+            total_realisasi_output_keseluruhan += total_realisasi_output_prog
 
-        # Tambahkan angka untuk program
-        prog_number = program_counter
-        program_counter += 1
-
-        prog_data.append({
-            'prog_number': prog_number,  # Nomor program dengan angka
-            'prog': prog,
-            'kegs': prog_kegs,
-            'total_pagu_prog': total_pagu_prog,
-            'total_output_prog': total_output_prog,
-            'total_realisasi_prog': total_realisasi_prog,
-            'total_realisasi_output_prog': total_realisasi_output_prog
-        })
+            program_counter += 1  # Increment program counter
 
     return {
         'prog_data': prog_data,
@@ -380,7 +363,5 @@ def get_data_context(request):
         'total_realisasi_output_keseluruhan': total_realisasi_output_keseluruhan,
         'tahunrealisasi': realisasi_tahun,
         'danarealisasi_id': Subkegiatan.objects.get(pk=realisasi_dana),
-        'tahaprealisasi_id': TahapDana.objects.get(pk=realisasi_tahap),
-        'subopdrealisasi_id': Subopd.objects.get(pk=realisasi_subopd),
         'jadwal': jadwal
     }
