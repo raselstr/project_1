55dfc707989d1e4be752d7360b9ae97aba985672
Rasel S Str <54805845+raselstr@users.noreply.github.com>
2026-02-06 21:59:05 +0700

    SP2D SIPD di Realisasi

--- sipd/urls.py
+++ sipd/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("progress/<str:task_id>/", views.sipd_import_progress, name="sipd-progress"),
     path("skipped/<int:tahun>/", views.skipped_sipd_view, name="sipd-skipped"),
     path("export/", views.export_sipd_excel, name="export_sipd_excel"),
+    path("dokumen/<str:mode>/<int:pk>/", views.view_sipd, name="data_sipd"),
+   
 ]
